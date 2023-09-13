555b2abe28c1133ab004d3876ef5ed184b309c73
ConduraruGheorghita <conduraru.gheorghita@gmail.com>
2023-09-14 02:27:13 +0300

    Update hello_world_test.cpp

--- Lectia_1/hello_world_test.cpp
+++ Lectia_1/hello_world_test.cpp
@@ -12,3 +12,15 @@ TEST_CASE("test_hello")
     // Check if your function returns "Hello, World!".
     REQUIRE(hello_world::hello() == "Hello, World!");
 }
+
+//Codul pe care l-ai furnizat face parte dintr-un test pentru funcția hello() definită în "hello_world.cpp". Acesta utilizează un framework de testare numit "Catch2" pentru a crea și executa teste pentru codul tău. Extensia fisierului în sine nu este importantă în acest context, dar convențional, astfel de fișiere de testare au extensia .cpp.
+
+// Iată câteva observații legate de tipul și extensia fișierului folosit pentru testare:
+
+// Extensie de fișier: De obicei, fișierele care conțin teste au extensia .cpp, pentru a le diferenția clar de fișierele header sau sursă. Cu toate acestea, nu există o regulă strictă pentru extensia fișierului de testare, și aceasta poate varia în funcție de convenția folosită în proiectul tău sau în mediul de dezvoltare. Ceea ce contează cel mai mult este conținutul și organizarea testelor în interiorul fișierului.
+
+// Framework de testare: Codul tău utilizează framework-ul de testare "Catch2", care este un instrument puternic și flexibil pentru a crea și rula teste unitare în C++. Acesta permite scrierea de teste cu expresii clare și ușor de citit, precum REQUIRE(hello_world::hello() == "Hello, World!");, și generează rapoarte detaliate despre rezultatele testelor.
+
+// Directiva preprocesorului: Observă utilizarea directivelor preprocesorului #ifdef și #else pentru a gestiona includerea corectă a framework-ului de testare. Acest lucru permite comutarea între utilizarea framework-ului "Catch2" (#include <catch2/catch.hpp>) și utilizarea unui alt framework personalizat (#include "test/catch.hpp") în funcție de contextul de dezvoltare sau de setările de compilare.
+
+// În esență, extensia fișierului în care sunt scrise testele poate varia, dar conținutul și framework-ul de testare pe care îl folosești sunt elementele esențiale pentru testarea corectă și eficientă a codului tău.
